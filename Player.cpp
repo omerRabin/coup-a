03c217331ec10b9ed5e7c45b3940b263529b3dcf
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-04-26 16:36:32 +0300

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,19 @@
+#include "Player.hpp"
+namespace coup{
+    void Player::income(){
+
+    }
+    void Player::foreign_aid(){
+
+    }
+    void Player::coup(Player player){
+
+    }
+    int Player::coins(){
+        return 1;
+    }
+    string Player::role(){
+        return "";
+    }
+    
+}
