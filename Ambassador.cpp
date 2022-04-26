6dd8d329a0b9439fc665058139af9c09935fa530
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-04-26 16:28:03 +0300

    Create Ambassador.cpp

---
+++ Ambassador.cpp
@@ -0,0 +1,9 @@
+#include "Ambassador.hpp"
+namespace coup{
+    void Ambassador::transfer(Player player1, Player player2){
+
+    }
+    void Ambassador::block(Player player){ // can block only Captain
+    } 
+    
+}
