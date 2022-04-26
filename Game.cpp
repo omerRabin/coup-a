a30642f4b15fcc03e7f2569521a7679dfcf67bc5
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-04-26 16:35:24 +0300

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,15 @@
+#include "Game.hpp"
+using namespace std;
+namespace coup{
+    string Game::turn(){
+        return "";
+    }
+    vector<string> Game::players(){
+        std::vector<std::string> thing;
+        return thing;
+    }
+    string Game::winner(){
+        return "";
+    }
+    
+}
