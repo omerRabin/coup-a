d5d3efcdf56e03b27027dd29827c3da5fe9a6c24
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-04-26 16:31:26 +0300

    Create Captain.cpp

---
+++ Captain.cpp
@@ -0,0 +1,10 @@
+#include "Captain.hpp"
+namespace coup{
+    void Captain::block(Player player){ // can block only captain
+
+    } 
+    void Captain::steal(Player player){
+        
+    }
+    
+}
