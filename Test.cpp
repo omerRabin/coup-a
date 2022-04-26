928cdf3c16a31c7a4de5c3a195e624b478051ceb
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-04-26 16:37:30 +0300

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//#include "Game.hpp"
+#include <stdexcept>
+#include "doctest.h"
+
+TEST_CASE("Test"){
+    for(int i=0; i<20; i++){
+        CHECK(1==1);
+    }
+}
+    
